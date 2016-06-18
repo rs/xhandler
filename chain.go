8323ade1cba70f6f5dc9095fcbbb4720ffdb28aa
DeepAnchor <xdeepanchor@gmail.com>
2016-06-18 11:23:55 -0700

    Extend Chain API with 'Add' and 'With' methods.
    'Add' is a variadic function allowing multiple (context or non context-aware) middleware handlers to be transparently added to a Chain (i.e chain.Add(xlog, xaccess, cors, ...). 'With' is a helper for creating a new, extended middleware Chain from an existing Chain, useful for route-specific handlers.

--- chain.go
+++ chain.go
@@ -10,6 +10,34 @@ import (
 // management.
 type Chain []func(next HandlerC) HandlerC
 
+// Add appends a variable number of additional middleware handlers
+// to the middleware chain. Middleware handlers can either be
+// context-aware or non-context aware handlers with the appropriate
+// function signatures.
+func (c *Chain) Add(f ...interface{}) {
+	for _, h := range f {
+		switch v := h.(type) {
+		case func(http.Handler) http.Handler:
+			c.Use(v)
+		case func(HandlerC) HandlerC:
+			c.UseC(v)
+		default:
+			panic("Adding invalid handler to the middleware chain")
+		}
+	}
+}
+
+// With creates a new middleware chain from an existing chain,
+// extending it with additional middleware. Middleware handlers
+// can either be context-aware or non-context aware handlers
+// with the appropriate function signatures.
+func (c *Chain) With(f ...interface{}) *Chain {
+	n := make(Chain, len(*c))
+	copy(n, *c)
+	n.Add(f...)
+	return &n
+}
+
 // UseC appends a context-aware handler to the middleware chain.
 func (c *Chain) UseC(f func(next HandlerC) HandlerC) {
 	*c = append(*c, f)
